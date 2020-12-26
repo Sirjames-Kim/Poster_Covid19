f0e6e2145aa687e04e34e65ab09ff035c1e9ae63
SEOKMIN KIM <bear900410@gmail.com>
2020-12-26 16:11:53 +0900

    Add files via upload

---
+++ code/poster_covid19.R
@@ -0,0 +1,44 @@
+library(bsts)
+library(CausalImpact)
+
+#Data
+dat <- read.csv("poster_df.csv", head = T)
+str(dat)
+##dat$mask <- log(dat$mask) # If log?
+
+#model
+
+post.period <- c(36, 50)
+post.period.response <- dat$mask[post.period[1] : post.period[2]] 
+dat$mask[post.period[1] : post.period[2]] <- NA
+
+ss1 <- AddLocalLinearTrend(list(), dat$mask)
+ss1[[1]][[2]]$prior.guess<-10 #Prior setting
+ss1[[1]][[3]]$prior.guess<-10 #Prior setting
+
+
+
+fit_bsts <- bsts(dat$mask ~ 
+                     dat$cum_confirmed+dat$cum_death+dat$confirmed+
+                     dat$world_cum_confirmed+dat$world_cum_death+dat$world_confirmed+
+                     dat$social_distance+dat$hand_sanitizer+dat$corona_virus+dat$anxiety_index 
+                   , state.specification = ss1, data = dat, niter = 5000) #model bsts fitting
+impact <- CausalImpact(bsts.model = fit_bsts,
+                       post.period.response = post.period.response, alpha = 0.05) 
+
+
+
+#plot
+plot(fit_bsts)
+plot(fit_bsts,"coef")
+plot(impact)
+summary(fit_bsts)
+
+#summary
+summary(impact)
+summary(impact, "report")
+impact$summary
+impact$series
+
+
+
